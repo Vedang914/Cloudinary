6761da5352affbfe975114e4bd348d43aa3d4068
vedang nagar <vedang914@gmail.com>
2024-06-13 20:52:02 +0530

    Image Upload route completed

--- controllers/localFileUpload.js
+++ controllers/localFileUpload.js
@@ -1,6 +1,7 @@
 const File = require("../models/fileModels");
+const cloudinary = require("cloudinary").v2;
 
-const localFileUpload = async (req, res) => {
+exports.localFileUpload = async (req, res) => {
   try {
     const file = req.files.file;
 
@@ -22,4 +23,77 @@ const localFileUpload = async (req, res) => {
   }
 };
 
-module.exports = { localFileUpload };
+function isFileTypeSupport(type,supportedTypes){
+
+  return supportedTypes.includes(type);
+
+}
+
+async function uploadFileToCloudinary(file,folder){
+  const options = {folder};
+
+  return await cloudinary.uploader.upload(file.tempFilePath,options)
+}
+
+
+exports.imageUpload = async(req,res)=>{
+  try{
+
+    const {name,tags,email} = req.body;
+    // console.log(name,tags,email)
+
+    const file = req.files.imageFile
+    // console.log(file)
+
+    //Validation
+    const supportedTypes = ["jpg","jpeg","png"]
+    const fileType = file.name.split(".")[1].toLowerCase();
+
+    console.log(fileType)
+
+    if(!isFileTypeSupport(fileType,supportedTypes)){
+
+      return res.status(400).json({
+        success:false,
+        message:"File type not supported"
+      })
+
+    }
+
+    const response = await uploadFileToCloudinary(file,"Gallery")
+    console.log(response);
+
+
+    const fileData = await File.create({
+      name,
+      tags,
+      email,
+      imageUrl:response.secure_url
+    })
+
+    res.status(200).json({
+      success:true,
+      
+      message:"File uploaded successfully"
+    })
+
+
+
+
+
+
+
+
+
+
+  } catch(error){
+    res.status(500).json({
+      success:false,
+      message:"something went wrong"
+    })
+  }
+}
+
+
+
+
--- routes/fileUploadRoutes.js
+++ routes/fileUploadRoutes.js
@@ -2,9 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
-const {localFileUpload} = require("../controllers/localFileUpload");
+const {localFileUpload, imageUpload} = require("../controllers/localFileUpload");
 
 router.post("/localFileUpload",localFileUpload);
+router.post("/imageUpload",imageUpload)
 
 
 module.exports = router;
